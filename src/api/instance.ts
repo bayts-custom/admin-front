397a43fcf20339ae98ce6d026aa826f7ac66cb8c
Tonia Ivanova <tonia.ivanova.2000@gmail.com>
2025-10-26 00:07:40 +0400

    Updates

--- src/api/instance.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-    timeout: 10000,
-    headers: { 'X-Custom-Header': 'foobar' },
-});
--- src/helpers/show-price.ts
+++
@@ -1,6 +0,0 @@
-export const showPrice = (price?: number): string => {
-    if (!price) {
-        return 'N/A';
-    }
-    return `${price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}k`;
-}
