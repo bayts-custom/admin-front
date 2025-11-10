2cad50ee211889b284a1a1d8dc5367c53581b6ba
Tonia Ivanova <tonia.ivanova.2000@gmail.com>
2025-11-11 00:58:20 +0400

    fix: change cors

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:1488',
-    // baseURL: 'https://bayts-server.onrender.com',
+    // baseURL: 'http://localhost:1488',
+    baseURL: 'https://bayts-server.onrender.com',
     timeout: 10000,
     headers: { 'X-Custom-Header': 'foobar' },
 });
